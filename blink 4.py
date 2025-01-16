961228900e688eddc42d8dc8f0b67c88131cab4e
aherfue0406 <aherfue0406@g.educaand.es>
2025-01-16 12:36:27 +0100

    Cuarto programa de blink.py

--- blink 4.py
+++ blink 4.py
@@ -1,32 +1,35 @@
 import pygame as pg
+from timer event import TimerEvent
 import time
 
+contador = 1 
 funcionando = True
 estado = False
 color = [(220,220,0), (50,50,50)]
 pg.init()
 screen = pg.display.set_mode((500, 500), 0 , 32)
-tiempoinicial = time.process_time()*1000
+
+def blink(packet):
+    global estado 
+    global t
+    estado = not(estado)
+
+te = Timer(interval=1.0)
+te.subscribe("Temporizador1_handler", blink)
 
 while funcionando:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             funcionando = False
             break
-    tiempoActual = time.process_time()*1000
-    if (tiempoActual - tiempoinicial) > 200:
-        '''
-        if estado:
-            color = [220,220,0]
-            estado = False
-        else:   
-            color = [50,50,50]
-            estado = True
-        '''
-        tiempoinicial = time.process_time()*1000
-        estado = not(estado)
+
 
     screen.fill([100, 100, 100])
     pg.draw.circle(screen, color[estado], (200, 200), 70)
     pg.display.flip() 
+     
+    print(contador, end='\r')
+    contador = contador+1
+
+t.cancel()
 pg.quit()
