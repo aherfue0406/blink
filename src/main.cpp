4966e6f9ebfe99d8a2d3db10593d7b0168a186f5
aherfue0406 <aherfue0406@g.educaand.es>
2025-03-18 09:24:04 +0100

    Proyecto milisegundos

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #define LED 13
 
+int contador = 0;
+
 void setup() {
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
@@ -9,10 +11,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED, HIGH);
-  delay(100);
+  
   digitalWrite(LED, LOW);
-  delay(100);
-    Serial.print("funciona.");
+
+    Serial.println(contador ++);
 }
 
 
